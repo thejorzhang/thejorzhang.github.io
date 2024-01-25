830c8c876928ac8453a7367c97e6e089c70822c6
thejorzhang <thejordanzhang@gmail.com>
2024-01-25 00:16:59 -0500

    added bootstrap page

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 
+var i = 0;
+var txt = 'Hey I\'m Jordan';
+var speed = 100;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("intro").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+function typeWriterOnLoad() {
+  // Call the typewriter function when the page loads
+  typeWriter();
+}
