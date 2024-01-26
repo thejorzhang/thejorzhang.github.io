ee371c5960b34ec5debca3743de33cf508326460
thejorzhang <thejordanzhang@gmail.com>
2024-01-25 23:27:56 -0500

    updated bootstrap page and changed to primary index

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 
 var i = 0;
-var txt = 'Hey I\'m Jordan';
 var speed = 100;
+var txt = 'Hey I\'m Jordan';
+var txt2 = 'I\'m a dedicated sophomore at the University of Maryland at College Park, pursuing finance and computer science. I enjoy web design and development and also have a deep interest in data science and analytics.';
+
+function typer(text) {
+  if (i < text.length) {
+    document.getElementById("intro").innerHTML += text.charAt(i);
+    i++;
+    setTimeout(function() {
+      typer(text);
+    }, speed);
+  }
+}
 
 function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("intro").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+  typer(txt);
+
+  // After typing txt, wait for 2 seconds and then type txt2
+  setTimeout(function() {
+    i = 0; // Reset i for typing txt2
+    document.getElementById("intro").innerHTML = '';
+    typer(txt2);
+  }, 1500);
 }
 
 function typeWriterOnLoad() {
   // Call the typewriter function when the page loads
   typeWriter();
 }
+
+// Call the typewriter function when the page loads
+typeWriterOnLoad();
