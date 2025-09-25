96fbd8675a75b347224e796731d999e7a3454ba9
thejorzhang <thejordanzhang@gmail.com>
2025-09-25 21:58:38 +0900

    Updated Projects

--- script.js
+++ script.js
@@ -46,3 +46,13 @@ function toggleIframe(id) {
 }
 
 
+function toggleElement(id) {
+  const el = document.getElementById(id);
+  if (el.style.display === 'none' || el.style.display === '') {
+    el.style.display = 'block';
+  } else {
+    el.style.display = 'none';
+  }
+}
+
+
