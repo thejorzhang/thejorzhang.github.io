da2e6e6458933bf614d54b43c65e7b338bf049a7
thejorzhang <thejordanzhang@gmail.com>
2026-03-02 22:27:59 -0500

    Formatting Updates

--- script.js
+++ script.js
@@ -1,39 +1,3 @@
-
-var i = 0;
-var speed = 100;
-var txt = 'Hey I\'m Jordan';
-var txt2 = 'I\'m a dedicated senior at the University of Maryland at College Park, pursuing finance and information systems. I enjoy web design and development and also have a deep interest in data science and analytics. Feel free to reach me at jorzhang@terpmail.umd.edu or (301)-273-5495.';
-
-function typer(text) {
-  if (i < text.length) {
-    document.getElementById("intro").innerHTML += text.charAt(i);
-    i++;
-    setTimeout(function() {
-      typer(text);
-    }, speed);
-  }
-}
-
-function typeWriter() {
-  typer(txt);
-
-  // After typing txt, wait for 2 seconds and then type txt2
-  setTimeout(function() {
-    i = 0; // Reset i for typing txt2
-    document.getElementById("intro").innerHTML = '';
-    typer(txt2);
-  }, 1500);
-}
-
-function typeWriterOnLoad() {
-  // Call the typewriter function when the page loads
-  typeWriter();
-}
-
-// Call the typewriter function when the page loads
-typeWriterOnLoad();
-
-
 function toggleIframe(id) {
   var iframe = document.getElementById(id);
 
@@ -54,5 +18,4 @@ function toggleElement(id) {
     el.style.display = 'none';
   }
 }
-
 
